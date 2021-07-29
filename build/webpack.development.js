bbe9c89872271121215d462f93383a29037037d6
RechieKho <the.real.richie.kho@gmail.com>
2021-07-29 05:19:37 -0700

    Add CSS Module functionality

--- build/webpack.development.js
+++ build/webpack.development.js
@@ -1,10 +1,19 @@
+const PostCSSLoader = require("../build.ext/css-ext-loader").PostCSSLoader
+const CSSLoader = require("../build.ext/css-ext-loader").CSSLoader
+const CSSModuleLoader = require("../build.ext/css-ext-loader").CSSModuleLoader
+
 module.exports = env => {
     return {
         module: {
             rules: [
                 {
                     test: /\.css$/,
-                    use: ["style-loader", "css-loader"]
+                    exclude: /\.module\.css$/,
+                    use: ['style-loader', CSSLoader, PostCSSLoader]
+                },
+                {
+                    test: /\.module\.css$/,
+                    use: ['style-loader', CSSModuleLoader, PostCSSLoader]
                 }
             ]
         }
--- build/webpack.production.js
+++ build/webpack.production.js
@@ -1,12 +1,21 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const PostCSSLoader = require("../build.ext/css-ext-loader").PostCSSLoader
+const CSSLoader = require("../build.ext/css-ext-loader").CSSLoader
+const CSSModuleLoader = require("../build.ext/css-ext-loader").CSSModuleLoader
 
 module.exports = env => {
     return {
         module:{
             rules: [{
                 test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader]
-            }]
+                exclude: /\.module\.css$/,
+                use: [MiniCssExtractPlugin.loader, CSSLoader, PostCSSLoader ]
+            },
+            {
+                test: /\.module\.css$/,
+                use: [MiniCssExtractPlugin.loader, CSSModuleLoader, PostCSSLoader]
+            }
+        ]
         },
         plugins: [new MiniCssExtractPlugin()]
     }
