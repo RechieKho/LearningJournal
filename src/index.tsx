b6dbdfb7b3f2e616f27bd425f09bd8c6b9e2dde4
RechieKho <the.real.richie.kho@gmail.com>
2021-07-30 00:28:52 -0700

    Add Styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
+import "@Styles/reset.css";
+import "@Styles/setup.css";
 import App from "./App";
 
 render(
---
+++ src/styles/modules/App.module.css.d.ts
@@ -0,0 +1,1 @@
+export const backgroundRed: string;
