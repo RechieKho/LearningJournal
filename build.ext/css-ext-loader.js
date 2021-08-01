a9bdd0e2fb3f6cfb1c28abaa16cc26c68d2dcc59
RechieKho <the.real.richie.kho@gmail.com>
2021-08-01 01:11:40 -0700

    Extract postcss configuration from ./build.ext/css-ext-loader.js

--- build.ext/css-ext-loader.js
+++ build.ext/css-ext-loader.js
@@ -1,40 +1,40 @@
-const autoprefixer = require('autoprefixer')
+const autoprefixer = require("autoprefixer");
 module.exports = {
-    CSSModuleLoader : {
-        loader: 'css-loader',
-        options: {
-            modules: {
-                mode: 'local',
-                exportLocalsConvention: "camelCase",
-                localIdentName: '[name]_[local]_[hash:base64:5]',
-            },
-            importLoaders: 2,
-            sourceMap: false,
-        }
+  CSSModuleLoader: {
+    loader: "css-loader",
+    options: {
+      modules: {
+        mode: "local",
+        exportLocalsConvention: "camelCase",
+        localIdentName: "[name]_[local]_[hash:base64:5]",
+      },
+      importLoaders: 2,
+      sourceMap: false,
     },
-    CSSLoader : {
-        loader: 'css-loader',
-        options: {
-            modules: {
-                mode: 'global',
-                exportLocalsConvention: "camelCase"
-            },
-            importLoaders: 2,
-            sourceMap: false,
-        }
+  },
+  CSSLoader: {
+    loader: "css-loader",
+    options: {
+      modules: {
+        mode: "global",
+        exportLocalsConvention: "camelCase",
+      },
+      importLoaders: 2,
+      sourceMap: false,
     },
-    PostCSSLoader : {
-        loader: 'postcss-loader',
-        options: {
-            sourceMap: false,
-            postcssOptions:{
-                plugins: () => [
-                    autoprefixer({
-                        browsers: ['>1%', 'last 4 versions', 'Firefox ESR', 'not ie < 9']
-                    })
-                ],
-                ident: 'postcss'
-            },
-        }
-    }   
-}
+  },
+  PostCSSLoader: {
+    loader: "postcss-loader",
+    options: {
+      sourceMap: false,
+      postcssOptions: {
+        // plugins: () => [
+        //     autoprefixer({
+        //         browsers: ['>1%', 'last 4 versions', 'Firefox ESR', 'not ie < 9']
+        //     })
+        // ],
+        ident: "postcss",
+      },
+    },
+  },
+};
